e9bb4d2cf6aef4482d930be3adc5b08c9c2c07f5
JIN <wlsqkrtk11@naver.com>
2025-04-16 17:51:37 +0900

    1~2레벨 끝냈습니다.

---
+++ src/main/java/org/example/expert/client/dto/WeatherDto.java
@@ -0,0 +1,14 @@
+package org.example.expert.client.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+
+public class WeatherDto {
+
+    private final String date;
+    private final String weather;
+
+}
